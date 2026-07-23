82f3aa2725c6348ff0c6fb099f86309387696b4b
hyazhar <uddinmdazhar21@gmail.com>
2026-07-23 14:12:08 +0530

    Implement Navbar, Footer, and Landing Page

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const engine= require('ejs-mate');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // View Engine
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs",engine);
 app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
     res.render("landingpage");
 });
+
 
 module.exports = app;
