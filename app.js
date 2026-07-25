28cbc75eeee5705f848ecd4142fdd7d5f3870fc3
hyazhar <uddinmdazhar21@gmail.com>
2026-07-25 12:11:01 +0530

    Add Search Functionality and Explore Listings

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const engine= require('ejs-mate');
 const ExpressError = require("./utils/ExpressError");
+const Listing=require('./models/listingSchema');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +13,33 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.engine("ejs",engine);
 app.set("view engine", "ejs");
-
 // Routes
 app.get("/", (req, res) => {
     res.render("landingpage");
+});
+
+app.get("/listings", async (req, res, next) => {
+    try {
+        const search = req.query.search;
+        let listings;
+
+        if (search) {
+            listings = await Listing.find({
+                $or: [
+                    { title: { $regex: search, $options: "i" } },
+                    { location: { $regex: search, $options: "i" } },
+                    { country: { $regex: search, $options: "i" } }
+                ]
+            });
+        } else {
+            listings = await Listing.find({});
+        }
+
+        res.render("explorelisting", { listings, search });
+
+    } catch (err) {
+        next(err);
+    }
 });
 
 // Errr Route
